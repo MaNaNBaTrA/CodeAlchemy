4207997e82516c883bd800d724b11a6c37547741
Manan Batra <mananbatra162004@gmail.com>
2024-09-27 22:04:09 +0530

    Initial commit

---
+++ CodeAlchemy/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [ 
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    colors: {
+      Text: '#545454',
+      Highlight : '	#AAFF00',
+      Menu : '#B6FA8E'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
