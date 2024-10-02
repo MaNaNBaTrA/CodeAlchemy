9c2819dece0251cd6743d72216ee53dcc5663cec
Manan Batra <mananbatra162004@gmail.com>
2024-10-03 02:38:27 +0530

    Updated Projects

--- CodeAlchemy/src/components/Projects.jsx
+++ CodeAlchemy/src/components/Projects.jsx
@@ -1,31 +1,42 @@
 import React, { useState, useEffect, useRef } from "react";
+import Bike from "../assets/Projects/BikePalace.png"
+import Blog from "../assets/Projects/Blog.png"
+import Booking from "../assets/Projects/Booking.png"
+import Book from "../assets/Projects/BookStore.png"
+import Port from "../assets/Projects/Portfolio.png"
+import Github from '/Svg/Github.svg'
 
 const Projects = () => {
   const [active, setActive] = useState(2);
   const [Tran, setTran] = useState(15);
-  const [Position, setPosition] = useState('calc(40% - 80px)');
+  const [Position, setPosition] = useState('');
   const itemsRef = useRef([]);
 
   const items = [
     {
-      title: "Project 1",
-
+      title: "Bike Palace",
+      img:Bike,
+      source:"https://github.com/MaNaNBaTrA/Bike-Palace"
     },
     {
-      title: "Project 2",
-      
+      title: "Bolg Website",
+      img:Blog,
+      source:"https://github.com/MaNaNBaTrA/Blog-Web"
     },
     {
-      title: "Project 3",
-      
+      title: "Art In Motion",
+      img:Port,
+      source:"https://github.com/MaNaNBaTrA/Art-In-Motion"
     },
     {
-      title: "Project 4",
-     
+      title: "Books Library",
+      img:Book,
+      source:"https://github.com/MaNaNBaTrA/Books-Library"
     },
     {
-      title: "Project 5",
-      
+      title: "Booking.com",
+      img:Booking,
+      source:"https://github.com/MaNaNBaTrA/Booking-App"
     },
   ];
 
@@ -85,9 +96,13 @@ const Projects = () => {
     const handleResize = () => {
       const screenX = window.innerWidth;
       if (screenX < 520) {
-        setPosition('calc(30% - 15px)');
-      } else {
-        setPosition('calc(40% - 50px)');
+        setPosition('calc(30% - 50px)');
+      }
+      else if(screenX < 586){
+        setPosition('calc(45% - 110px)');
+      } 
+      else {
+        setPosition('calc(40% - 70px)');
       }
     };
 
@@ -107,7 +122,7 @@ const Projects = () => {
       if (screenX < 440) {
         setTran(6);
       } 
-      else if (screenX < 580) {
+      else if (screenX < 586) {
         setTran(8);
       } 
       else if (screenX < 682) {
@@ -132,17 +147,18 @@ const Projects = () => {
   
 
   return (
-    <div className="f relative mt-20 mb-20 w-full overflow-hidden px-12">
-      <div className="relative w-full h-[150px] overflow-hidden">
+    <div className="f relative mt-32 mb-20 w-full overflow-hidden px-12">
+      <div className="relative w-full h-[550px] overflow-hidden">
         {items.map((item, index) => (
           <div
             key={index}
             ref={(el) => (itemsRef.current[index] = el)}
-            className="absolute w-[30%] h-[300px] text-justify rounded-lg p-5 transition-all duration-500 max-[970px]:w-[40%] max-[800px]:w-[45%] max-[700px]:w-[50%] max-[650px]:w-[55%]"
+            className="absolute w-[30%] h-[600px] text-justify rounded-lg p-5 transition-all duration-500 max-[970px]:w-[40%] max-[800px]:w-[45%] max-[700px]:w-[50%] max-[650px]:w-[55%] flex flex-col max-[586px]:w-[70%]"
             style={{ left: Position, top: 0 }}
           >
-            <h1 className="text-xl font-bold mb-2">{item.title}</h1>
-            <p>{item.content}</p>
+            <h1 className="text-xl  mb-2 font-Grotesk max-[586px]:text-lg">{item.title}</h1>
+            <img src={item.img} alt={item.title + 'Img'} />
+           { active === index && <a  href={item.source} target="_blank" className="transition-all duration-1000 opacity-100  ml-1"><div className="inline-flex bg-Black text-White mt-4 rounded-xl gap-2 items-center justify-center p-1 cursor-pointer opacity-100 hover:scale-105 transition-all"><img src={Github} alt="Source" className="w-8 invert max-[586px]:w-6"/>Source</div></a>}
           </div>
         ))}
         <div
